8455ee08500c0ef1e3540ded645dbbc589851105
naveenjaiswal2023 <navinjaiswal005@outlook.com>
2025-04-12 10:30:58 +0530

    aaa

--- Infrastructure/AzureBus/AzureServiceBusBackgroundService.cs
+++ Infrastructure/AzureBus/AzureServiceBusBackgroundService.cs
@@ -5,15 +5,29 @@ namespace HospitalQueueSystem.Infrastructure.AzureBus
     public class AzureServiceBusBackgroundService : BackgroundService
     {
         private readonly IQueueSubscriber _subscriber;
+        private readonly ILogger<AzureServiceBusBackgroundService> _logger;
 
-        public AzureServiceBusBackgroundService(IQueueSubscriber subscriber)
+        public AzureServiceBusBackgroundService(
+            IQueueSubscriber subscriber,
+            ILogger<AzureServiceBusBackgroundService> logger)
         {
             _subscriber = subscriber;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await _subscriber.StartAsync(stoppingToken);
+            try
+            {
+                _logger.LogInformation("Starting Azure Service Bus Subscriber...");
+                await _subscriber.StartAsync(stoppingToken);
+                _logger.LogInformation("Azure Service Bus Subscriber started.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while starting Azure Service Bus Subscriber.");
+            }
         }
+
     }
 }
